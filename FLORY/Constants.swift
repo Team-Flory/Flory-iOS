9c0d87e06bdfc6b16d18482b3e9baa309952d337
gourderased <gourderased@gmail.com>
2023-11-01 18:38:51 +0900

    "fix : 가입하기 버튼 색 변경"

--- FLORY/Constants.swift
+++ FLORY/Constants.swift
@@ -1,5 +1,5 @@
 import Foundation
 
 enum Constants {
-    static let openAIApiKey = "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"
+    static let openAIApiKey = "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"
 }
